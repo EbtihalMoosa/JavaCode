c8ece992c118060e0797d807083fe93a1ed12fdd
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-07-30 12:45:10 +0400

    Perimeter of rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,31 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
-        {
-                Scanner read = new Scanner(System.in);
-                System.out.println("please answer this questions:");
-                System.out.println("enter your name");
-                String name = read.next();
-                System.out.println("enter number of question");
-                int num = read.nextInt();
 
 
+public class Main
 
-                String[] q= {"1+1= " ,"1+3= " , "3+3= ", "2+0= " , "5+3= "} ;
-                int [] a= {2 ,4 , 6, 2 , 8} ;
-                int total = 0;
-                for (int i=0; i<num; i++){
-                    System.out.println(q[i]);
-                    int answer = read.nextInt();
-                    if (answer == a[i]){
-                        System.out.println("True");
-                        total++;
-                    }else {
-                        System.out.println("False");}
-                }
-                System.out.println("the total "+ total);
-            }
-        }
+{
+ public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
 
+ System.out.print("Enter the width of the rectangle: ");
+ double width = scanner.nextDouble();
 
+ System.out.print("Enter the height of the rectangle: ");
+ double height = scanner.nextDouble();
 
+ // Calculate the area and perimeter of the rectangle
+ double area = width * height;
+ double perimeter = 2 * (width + height);
 
-            }
+ // Print the results
+ System.out.println("Area is " + width + " * " + height + " = " + area);
+ System.out.println("Perimeter is 2 * (" + width + " + " + height + ") = " + perimeter);
+
+ scanner.close();
+ }
+}
+
 
 
 
