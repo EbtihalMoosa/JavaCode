17efd12cb98661354ea66e9069555d4673a97aa7
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-07-30 12:39:29 +0400

    calculate the number of correct answers

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        {
+                Scanner read = new Scanner(System.in);
+                System.out.println("please answer this questions:");
+                System.out.println("enter your name");
+                String name = read.next();
+                System.out.println("enter number of question");
+                int num = read.nextInt();
 
-    Scanner d = new Scanner(System.in);
-        String input = d.nextLine();
-        String[] word = input.split(" ");
 
 
+                String[] q= {"1+1= " ,"1+3= " , "3+3= ", "2+0= " , "5+3= "} ;
+                int [] a= {2 ,4 , 6, 2 , 8} ;
+                int total = 0;
+                for (int i=0; i<num; i++){
+                    System.out.println(q[i]);
+                    int answer = read.nextInt();
+                    if (answer == a[i]){
+                        System.out.println("True");
+                        total++;
+                    }else {
+                        System.out.println("False");}
+                }
+                System.out.println("the total "+ total);
+            }
+        }
 
 
-            System.out.println("reverse sentence");
-            for (int i = word.length- 1; i >= 0; i--) {
-                System.out.print(word[i] + " ");
 
 
             }
-        }}
+
 
 
 
