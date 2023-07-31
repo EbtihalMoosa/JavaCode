9b67dea3858505743de72ed7d22535d12f00c38e
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-07-31 08:10:18 +0400

    Swap
    java program that swap two variables

---
+++ src/swap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        int a = 12, b = 4;
+        int temp = a;
+        a=b;
+        b= temp;
+        System.out.println("a: " +a);
+        System.out.println("b: " +b);
+
+
+    }}
