73b931c5e676a982f0d1ce654e6658a3fcca0159
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-08-01 08:58:39 +0400

    Task 3
    1 - Problem: Implement pow(x, n), which calculates x raised to the power n (x^n).
    2 - Problem : Pc Will Guess A number and the user will choose a number then you will tell him if larger or less till he choose the right number.
    3 - Problem: Given a 2D board and a word, find if the word exists in the grid. The word can be constructed from letters of sequentially adjacent cells, where "adjacent" cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.

--- src/Task3.java
+++ src/Task3.java
@@ -1,38 +1,81 @@
 import java.util.Scanner;
-
 public class Task3 {
-    public static void main(String[] args) {
-        char[][] chars ={
-                {'a', 'b', 'c'},{'d','e','f'},{'j', 'h', 'i'}};
-//                String word= "abcdd";
-        Scanner read = new Scanner (System.in);
-        System.out.println("write word");
+        public static void main(String[] args) {
+//Task 1
+                double a = 1;
+
+                Scanner read = new Scanner(System.in);
+                System.out.print("enter number: ");
+                double n = read.nextDouble();
+
+                System.out.print("enter power: ");
+                double p = read.nextDouble();
+// اذا البور لايساوي الصفر
+                while (p != 0) {
+                        a = n * a;
+                        --p;
+                        System.out.println("=" + a);
+
+
+//Task2
+                        {
+//                                Scanner read = new Scanner(System.in);
+                                int number = 1 + (int) (100 * Math.random());
+                                int x = 10;
+                                int i;
+
+                                for (i = 0; i < x; i++) {
+                                        System.out.println("Guess a number between 1 to 100: ");
+                                        int guess = read.nextInt();
+
+                                        if (number == guess) {
+                                                System.out.println("Right Answer");
+                                                break;
+                                        } else if (number > guess) {
+                                                System.out.println("Wrong, Larger");
+                                        } else if (number < guess) {
+                                                System.out.println("Wrong, Less ");
+                                        }
+                                }
+                        }
+                }
+        }
+
+// Task 3
+
+//public class Task3 {
+//    public static void main(String[] args) {
+
+        char[][] chars = {
+                {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'j', 'h', 'i'}};
+        //                String word= "abcdd";
+        Scanner read = new Scanner(System.in);
+                System.out.println("write word");
         String word = read.next();
 
-                char[] cs = word.toCharArray();
-                boolean isThisWordValid = true;
-                for (char c :cs) {
-                    System.out.println(c + " : " + isExist(chars,c));
-                    if (!isExist(chars,c)) {
-                         isThisWordValid=false;
+        char[] cs = word.toCharArray();
+        boolean isThisWordValid = true;
+                for(
+        char c :cs)
+
+        {
+                System.out.println(c + " : " + isExist(chars, c));
+                if (!isExist(chars, c)) {
+                        isThisWordValid = false;
                         break;
-                    }
+                }
+        }
+        // while (isExist(chars, c)){
+}
+static boolean isExist(char[][] chars, char c){
+        for (int i=0; i < chars.length; i++) {
+        for (int j = 0; j < chars[0].length; j++) {
+        if (c == chars[i][j]) {
+        return true;
+        }
 
         }
-       // while (isExist(chars, c)){
+        }
 
-
-    }
-
-        static boolean isExist(char[][] chars, char c){
-            for (int i=0; i < chars.length; i++) {
-                    for (int j = 0; j < chars[0].length; j++) {
-                        if (c == chars[i][j]) {
-                            return true;
-                        }
-
-                    }
-                }
-
-                return false;
-            }}
+        return false;
+        }}
