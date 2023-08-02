ad0c6b7fc07c51cd0cf2fd74b5aff598f00b3366
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-08-02 16:35:53 +0400

    task 4
    3 - Problem: Given an array of integers nums, sort the array in ascending order.
    
    Example:
    Input: [5, 2, 3, 1]
    Output: [1, 2, 3, 5]
    
    
    
    4 - Problem: Given an unsorted integer array nums, find the smallest missing positive integer.
    
    Example:
    Input: [3,4,-1,1]
    Output: 2 (The smallest missing positive integer is 2.)

---
+++ src/Task4.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+
+public static void main(String[] args) {
+Scanner read = new Scanner(System.in);
+//Task 1
+ System.out.println("please enter array count");
+ int n = read.nextInt();
+
+// //Todo Input
+ int[] x = new int[n];
+ for (int i = 0; i < n; i++) {
+ System.out.print("please enter number : ");
+ x[i] = read.nextInt();
+ }
+
+ int[] y = new int[n];
+
+ for (int num:x) {
+ if (num == i) {
+     System.out.println();
+
+
+
+//todo Task2
+//    int[] numbers = { 3 , 4 , -1 , 1};
+//
+//    int[] positiveNumbers = new int[numbers.length];
+//    //for each
+//    for (int x : numbers) {
+//        if (x < 0) {
+////            numbers.clone();
+//        }
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] < 0) {
+//                numbers[i] = 0;
+//            }
+//        }
+//
+//        Arrays.sort(numbers);
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (i != numbers[i]) {
+//                System.out.println(i);
+//                break;
+            }
+        }}}
+
+
+
+
+
